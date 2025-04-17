54163a93ffec85c79f6ee097dc5892f6617b9a57
Andrea.Parrini.Lavoro <andrea.parrini26@gmail.com>
2025-04-17 10:56:26 +0200

    add animation in a modal and add particolar components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 
 import NewChallenge from './NewChallenge.jsx';
+import { motion, AnimatePresence } from 'framer-motion';
 
 export default function Header() {
   const [isCreatingNewChallenge, setIsCreatingNewChallenge] = useState();
@@ -15,13 +16,24 @@ export default function Header() {
 
   return (
     <>
-      {isCreatingNewChallenge && <NewChallenge onDone={handleDone} />}
+      {/* 
+      Questo speciale compoentne fornito da fremer motion serve per dire far si che framer motion capisce che c'è un animazione in uscita 
+      e prima di rimuovere il compoente immediatamente effettua prima l'animazione 
+      */}
+      <AnimatePresence>
+        {isCreatingNewChallenge && <NewChallenge onDone={handleDone} />}
+      </AnimatePresence>
+      
 
       <header id="main-header">
         <h1>Your Challenges</h1>
-        <button onClick={handleStartAddNewChallenge} className="button">
+        <motion.button 
+          whileHover={{scale: 1.2}}
+          transition={{type: 'spring', stiffness: '500'}}
+          onClick={handleStartAddNewChallenge} 
+          className="button">
           Add Challenge
-        </button>
+        </motion.button>
       </header>
     </>
   );
