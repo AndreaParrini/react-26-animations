0ff90902b086686dd16a13373a070e13c9529a11
Andrea.Parrini.Lavoro <andrea.parrini26@gmail.com>
2025-04-18 11:05:03 +0200

    add animation on challenge page
    add animation in li\ol and p in challenges whit prop mode
    add animation on tab indicator in challengetab whit layoutid

--- src/components/Challenges.jsx
+++ src/components/Challenges.jsx
@@ -3,6 +3,7 @@ import { useContext, useState } from 'react';
 import { ChallengesContext } from '../store/challenges-context.jsx';
 import ChallengeItem from './ChallengeItem.jsx';
 import ChallengeTabs from './ChallengeTabs.jsx';
+import { AnimatePresence, motion } from 'framer-motion';
 
 export default function Challenges() {
   const { challenges } = useContext(ChallengesContext);
@@ -40,19 +41,38 @@ export default function Challenges() {
         onSelectType={handleSelectType}
         selectedType={selectedType}
       >
-        {displayedChallenges.length > 0 && (
-          <ol className="challenge-items">
-            {displayedChallenges.map((challenge) => (
-              <ChallengeItem
-                key={challenge.id}
-                challenge={challenge}
-                onViewDetails={() => handleViewDetails(challenge.id)}
-                isExpanded={expanded === challenge.id}
-              />
-            ))}
-          </ol>
-        )}
-        {displayedChallenges.length === 0 && <p>No challenges found.</p>}
+        {/* Quando all'interno di un componente AnimatePresence ci sono più componenti, si deve indicare anche una proprietà chiamata key
+        poi per non sincronizzare l'usicta a video dei due compoenenti si deve impostare la modalità su 'wait*/}
+        <AnimatePresence mode='wait'>
+          {displayedChallenges.length > 0 && (
+            <motion.ol key='list' 
+              initial={{y: -20, opacity: 0}} 
+              animate={{opacity: 1, y: 0}} 
+              exit={{y: -30, opacity:0}} 
+              className="challenge-items"
+            >
+              <AnimatePresence>
+                {displayedChallenges.map((challenge) => (
+                  <ChallengeItem
+                    key={challenge.id}
+                    challenge={challenge}
+                    onViewDetails={() => handleViewDetails(challenge.id)}
+                    isExpanded={expanded === challenge.id}
+                  />
+                ))}
+              </AnimatePresence>
+            </motion.ol>
+          )}
+          
+          {displayedChallenges.length === 0 && 
+            <motion.p 
+              key='fallback' 
+              initial={{y: -20, opacity: 0}} 
+              animate={{opacity: 1, y: 0}}
+              exit={{y: -20, opacity: 0}}>
+                No challenges found.
+              </motion.p>}
+        </AnimatePresence>
       </ChallengeTabs>
     </div>
   );
